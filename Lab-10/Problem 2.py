dfb414adf966a7b388a50eb45e82b19a4ee3695a
Vrajj0304 <24bit050@sot.pdpu.ac.in>
2025-04-23 12:33:40 +0530

    Update Problem 2.py

--- Lab-10/Problem 2.py
+++ Lab-10/Problem 2.py
@@ -1,5 +1,3 @@
-# Read the data stored in MS-Excel file and convert it into a dictionary. The record contains rollno, name of student, marks of three subjects. Also calculate total. Display the dictionary data on the monitor.
-
 import csv
 
 with open("Q2.csv", "r") as f:
